43e8e503e38d8e2c87a4d94ecea29f90eed61c3d
lrod <rleslie1015@gmail.com>
2019-03-06 14:51:53 -0800

    completed this,js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window
+* 2. implicit
+* 3. new bindin
+* 4. explicit binding
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,51 @@
 // Principle 1
 
 // code example for Window Binding
+function doesNothing(something) {
+    console.log(this);
+    return something;
+  }
+  doesNothing("nope");
 
-// Principle 2
+  // Principle 2
 
 // code example for Implicit Binding
+const robot = {
+    greeting: 'bleep blop',
+    compute: function(instruction) {
+      console.log(`${this.greeting} I will ${instruction} for you`);
+    }
+  };
+  robot.compute('dance');
+
 
 // Principle 3
 
 // code example for New Binding
-
+function SuperHero(name, power) {
+    this.name = name;
+    this.power= power;
+    this.saveTheDay = function() {
+      console.log(`${this.name} has saved the day with his ${this.power}`);
+  
+    };
+  }
+  
+  const ironman = new SuperHero ('Ironman', 'SUPERSTRENGH');
+  const venom = new SuperHero ('Venom', 'alien alterego');  
+  
+  ironman.saveTheDay();
+  venom.saveTheDay();
 // Principle 4
 
 // code example for Explicit Binding
+
+
+const villian = {
+    name: "carnage"
+}
+function evilLaugh() {
+    return `muahhahahaha ITS ME ${this.name}`;
+}
+
+console.log(evilLaugh.apply(villian))
